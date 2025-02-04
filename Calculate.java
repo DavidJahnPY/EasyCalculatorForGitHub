7cbb4a02818c6d339fda0f3acbf6931e34852537
DavidJahnPY <davidajahn11042007@gmail.com>
2025-02-04 16:20:27 +0100

    third figure to calculate with is added(do not look at the code without protection you might could regret doing so)

--- Calculate.java
+++ Calculate.java
@@ -1,21 +1,81 @@
 public class Calculate{
-    public Calculate(double pnumber1, String psign, double pnumber2){
-        System.out.println("you want to: " + pnumber1 + " " + psign + " " + pnumber2);
+    
+    public Calculate(double pnumber1, String psign1, double pnumber2, String psign2, double pnumber3){
+        System.out.println("you want to: " + pnumber1 + " " + psign1 + " " + pnumber2 + " " + psign2 + " " + pnumber3);
         
         double result = 0;
-        if(psign.equals("+")){
-            result = pnumber1 + pnumber2;
+        if(psign1.equals("*") || psign1.equals("/") || psign2.equals("*") || psign2.equals("/")){
+            if(psign1.equals("*") && psign2.equals("*") || psign1.equals("*") && psign2.equals("/") ||  psign1.equals("/") && psign2.equals("*") || psign1.equals("/") && psign2.equals("/")              ){
+                if(psign1.equals("*")){
+                    result = pnumber1 * pnumber2;
+                }
+                else if(psign1.equals("/")){
+                    result = pnumber1 / pnumber2;
+                }
+                
+                if(psign2.equals("*")){
+                    result = result * pnumber3;
+                }
+                else if(psign2.equals("/")){
+                    result = result / pnumber3;
+                }
+            }
+            
+            else if(psign1.equals("*") || psign1.equals("/")){
+                System.out.println("here1");
+                if(psign1.equals("*")){
+                    System.out.println("here2");
+                    result = pnumber1 * pnumber2;
+                }
+                else if(psign1.equals("/")){
+                    result = pnumber1 / pnumber2;
+                }
+                
+                if(psign2.equals("+")){
+                    System.out.println("here3");
+                    result = result + pnumber3;
+                }
+                else if(psign2.equals("-")){
+                    result -= pnumber3;
+                }
+            }
+            else{
+                if(psign2.equals("*")){
+                    result = pnumber2 * pnumber3;
+                }
+                else if(psign2.equals("/")){
+                    result = pnumber2 / pnumber3;
+                }
+                
+                if(psign1.equals("+")){
+                    result += pnumber1;
+                }
+                else if(psign1.equals("-")){
+                    result = pnumber1 - result;
+                }
+            }
         }
-        else if(psign.equals("-")){
-            result = pnumber1 - pnumber2;
-        }
-        else if(psign.equals("*")){
-            result = pnumber1 * pnumber2;
-        }
-        else if(psign.equals("/")){
-            result = pnumber1 / pnumber2;
-        }
+        else{
+                if(psign1.equals("+")){
+                    result = pnumber1 + pnumber2;
+                }
+                else if(psign1.equals("-")){
+                    result = pnumber1 - pnumber2;
+                }
+                
+                if(psign2.equals("+")){
+                    result += pnumber3;
+                }
+                else if(psign2.equals("-")){
+                    result -= pnumber3;
+                }
+            }
         
         System.out.println("The result of the calculation is: " + result);
     }
+    
+    public void calc(){
+        
+    }
 }
+
--- main.java
+++ main.java
@@ -4,9 +4,11 @@ public class main{
         GainInput gainInput = new GainInput();
         
         double number1 = gainInput.getInputDouble("Please enter the first number for calculations: ");
-        String sign = gainInput.getInputString("Please enter whether you want to calculate with '+', '-', '*', '/': ");
+        String sign1 = gainInput.getInputString("Please enter whether you want to calculate with '+', '-', '*', '/': ");
         double number2 = gainInput.getInputDouble("Please enter the second number for calculations: ");
+        String sign2 = gainInput.getInputString("Please enter whether you want to calculate with '+', '-', '*', '/': ");
+        double number3 = gainInput.getInputDouble("Please enter the third number for calculations: ");
         
-        Calculate calculate = new Calculate(number1, sign, number2);
+        Calculate calculate = new Calculate(number1, sign1, number2, sign2, number3);
     }
 }
